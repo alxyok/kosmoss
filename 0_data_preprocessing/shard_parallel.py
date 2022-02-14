fcf4edefe4ff946fbff7241f95ff8bbab2c9005a
alxyok <0xa1e4@gmail.com>
2022-02-14 11:43:37 +0000

    tmp

--- 0_data_preprocessing/shard_parallel.py
+++ 0_data_preprocessing/shard_parallel.py
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (c) 2022 alxyok
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,4 +20,37 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import 
+import h5py
+from mpi4py import MPI
+import numpy as np
+import os.path as osp
+
+import config
+
+
+def main():
+    
+    h5_path = osp.join(config.processed_data_path, f'feats-{step}.h5')
+
+    rank = MPI.COMM_WORLD.rank
+
+    for subidx in np.range(53):
+
+        # start = rank * (2 ** 4 * (1 + subidx))
+        # end = start + 2 ** 4
+        start = rank * 53 * 2 ** 4 + subidx * 2 ** 4 
+        end = start + 2 ** 4
+
+        with h5py.File(h5_path, 'r', driver='mpio', comm=MPI.COMM_WORLD) as feats:
+
+            sharded_path = osp.join(config.processed_data_path, f'feats-{step}.{rank}.{subidx}.h5')
+            with h5py.File(sharded_path, 'w', driver='mpio', comm=MPI.COMM_WORLD) as sharded:
+
+                f.create_dataset("x", data=feats['/x'][start:end])
+                f.create_dataset("y", data=feats['/y'][start:end])
+                f.create_dataset("edge", data=feats['/edge'][start:end])
+                
+                
+if __name__ == "__main__":
+    
+    main()

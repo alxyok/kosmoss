855e23555da6307042bc91f3bdec10ca0c2484f2
alxyok <0xa1e4@gmail.com>
2022-02-15 22:19:05 +0000

    updated with recommendations from Lightning Doc on multi-GPU

--- 1_multi_gpu_training/data.py
+++ 1_multi_gpu_training/data.py
@@ -20,134 +20,67 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import h5py
-from memory_profiler import profile
-import networkx as nx
 import numpy as np
-import os
 import os.path as osp
 import pytorch_lightning as pl
-from pytorch_lightning.utilities.cli import DATAMODULE_REGISTRY
 import torch
-import torch.nn.functional as F
-import torch_geometric as pyg
-import torch_optimizer as optim
-import yaml
-import netCDF4
-from tqdm import tqdm
+from typing import Tuple, Union
+import sys
+
+sys.path.append(osp.abspath('..'))
 
 import config
-from build_graphs import BuildGraphsFlow
-
-# class GraphDataset(torch.utils.data.Dataset):
-    
-#     def __init__(self, mode="train"):
-#         super().__init__()
-        
-#         directed_index = np.array([[*range(1, 138)], [*range(137)]])
-#         undirected_index = np.hstack((
-#             directed_index, 
-#             directed_index[[1, 0], :]
-#         ))
-#         self.undirected_index = torch.tensor(undirected_index, dtype=torch.long)
-    
-#     def raw_file_names(self):
-#         return None
-    
-#     def processed_file_names(self):
-#         return [f"data-{config.params['timestep']}.{k}.pt" 
-#                 for k in np.arange(config.params['num_shards'])]
-    
-#     def download(self):
-#         pass
-    
-#     def process(self):
-#         BuildGraphsFlow()
-
-# class GraphDataset(pyg.data.Dataset):
-    
-#     def __init__(self, mode="train"):
-#         super().__init__()
-        
-#         directed_index = np.array([[*range(1, 138)], [*range(137)]])
-#         undirected_index = np.hstack((
-#             directed_index, 
-#             directed_index[[1, 0], :]
-#         ))
-#         self.undirected_index = torch.tensor(undirected_index, dtype=torch.long)
-    
-#     def __len__(self):
-#         return config.params['dataset_len']
-    
-#     def __getitem__(self, idx):
-#         fileidx = idx // config.params['num_shards']
-#         rowidx = idx % config.params['num_shards']
-        
-#         path = osp.join(config.processed_data_path, f"feats-{config.params['timestep']}", f'{fileidx}.npy')
-#         feats = np.memmap(
-#             path, 
-#             dtype = config.params['dtype'],
-#             mode='r',
-#             shape=config.params['shard_shape']
-#         )
-        
-#         x = torch.squeeze(torch.tensor(feats[rowidx, :, :20]))
-#         y = torch.squeeze(torch.tensor(feats[rowidx, :, 20:]))
-
-#         graph = pyg.data.Data(x=x, edge_index=self.undirected_index, y=y)
-        
-#         return graph
 
 class FlattenedDataset(torch.utils.data.Dataset):
     
-    def __init__(self, step):
+    def __init__(self, step: int) -> None:
         super().__init__()
         self.step = step
+        self.params = config.params[str(self.step)]['flattened']
     
-    def __len__(self):
+    def __len__(self) -> int:
         
-        return config.params[str(step)]['flattened']['dataset_len']
+        return self.params['dataset_len']
     
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor]:
         
-        fileidx = idx // config.params['num_shards']
-        rowidx = idx % config.params['num_shards']
+        shard_size = len(self) // self.params['num_shards']
+        fileidx = idx // shard_size
+        rowidx = idx % shard_size
         
-        def load(name):
-            main_path = osp.join(config.processed_data_path, f"flattened-{self.timestep}")
-            data = np.memmap(
-                osp.join(main_path, name, f'{fileidx}.npy'), 
-                dtype = config.params['dtype'],
-                mode='r',
-                shape=config.params['flattened'][f'{name}_shape']
-            )
+        def _load(name: Union['x', 'y']) -> Tuple[torch.Tensor]:
+            main_path = osp.join(config.processed_data_path, f"flattened-{self.step}")
+            # data = np.memmap(
+            #     osp.join(main_path, name, f'{fileidx}.npy'), 
+            #     dtype = self.params['dtype'],
+            #     mode='r',
+            #     shape=self.params[f'{name}_shape']
+            # )
+            data = np.load(osp.join(main_path, name, f'{fileidx}.npy'))
             tensor = torch.squeeze(torch.tensor(data[rowidx, ...]))
             return tensor
         
-        x = load('x')
-        y = load('y')
+        x = _load('x')
+        y = _load('y')
         
         return x, y
     
 
-
-@DATAMODULE_REGISTRY
 class FlattenedDataModule(pl.LightningDataModule):
-    """Creates datasets for the """
     
     def __init__(self, 
                  batch_size: int,
-                 num_workers: int):
+                 num_workers: int) -> None:
         
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.timestep = config.config['timestep']
         super().__init__()
         
-    def prepare_data(self):
+    def prepare_data(self) -> None:
         pass
     
-    def setup(self, stage: str):
+    def setup(self, stage: str) -> None:
         dataset = FlattenedDataset(self.timestep)
         length = len(dataset)
         
@@ -159,94 +92,24 @@ class FlattenedDataModule(pl.LightningDataModule):
                 int(length * .1)
             ])
     
-    def train_dataloader(self):
+    def train_dataloader(self) -> torch.utils.data.DataLoader:
+        
         return torch.utils.data.DataLoader(
             self.train, 
             batch_size=self.batch_size, 
             shuffle=True, 
             num_workers=self.num_workers)
     
-    def val_dataloader(self):
+    def val_dataloader(self) -> torch.utils.data.DataLoader:
+        
         return torch.utils.data.DataLoader(
             self.val, 
             batch_size=self.batch_size, 
             num_workers=self.num_workers)
     
-    def test_dataloader(self):
+    def test_dataloader(self) -> torch.utils.data.DataLoader:
+        
         return torch.utils.data.DataLoader(
             self.test, 
             batch_size=self.batch_size, 
             num_workers=self.num_workers)
-
-# class RawDataset(torch.utils.data.Dataset):
-    
-#     def __len__(self):
-#         return config.params['dataset_len']
-    
-#     def __getitem__(self, idx):
-#         fileidx = idx // config.params['num_shards']
-#         rowidx = idx % config.params['num_shards']
-        
-#         path = osp.join(config.processed_data_path, f'feats-{config.params['timestep']}', f'{fileidx}.npy')
-#         feats = np.memmap(
-#             path, 
-#             dtype = config.params['dtype'],
-#             mode='r',
-#             shape=config.params['shape']
-#         )
-        
-#         x = torch.tensor(feats[rowidx, :, :20])
-#         y = torch.tensor(feats[rowidx, :, 20:])
-        
-#         return x, y
-
-
-# @DATAMODULE_REGISTRY
-# class LitThreeDCorrectionDataModule(pl.LightningDataModule):
-#     """Creates datasets for the """
-    
-#     def __init__(self, 
-#                  timestep: int,
-#                  batch_size: int,
-#                  num_workers: int):
-        
-#         self.batch_size = batch_size
-#         self.num_workers = num_workers
-#         super().__init__()
-        
-#     def prepare_data(self):
-#         GraphDataset()
-    
-#     def setup(self, stage):
-#         dataset = GraphDataset()#.shuffle()
-#         length = len(dataset)
-        
-#         # self.test_dataset = GraphDataset("test")
-#         # self.val_dataset = GraphDataset("val")
-#         # self.train_dataset = GraphDataset("train")
-        
-#         # self.test_dataset = dataset[int(length * .9):]
-#         # self.val_dataset = dataset[int(length * .8):int(length * .9)]
-#         # self.train_dataset = dataset[:int(length * .8)]
-#         self.train, self.val, self.test = torch.utils.data.random_split(
-#             dataset,
-#             [int(length * .8), int(length * .1), int(length * .1)])
-    
-#     def train_dataloader(self):
-#         return pyg.loader.DataLoader(
-#             self.train, 
-#             batch_size=self.batch_size, 
-#             shuffle=True, 
-#             num_workers=self.num_workers)
-    
-#     def val_dataloader(self):
-#         return pyg.loader.DataLoader(
-#             self.val, 
-#             batch_size=self.batch_size, 
-#             num_workers=self.num_workers)
-    
-#     def test_dataloader(self):
-#         return pyg.loader.DataLoader(
-#             self.test, 
-#             batch_size=self.batch_size, 
-#             num_workers=self.num_workers)
--- 1_multi_gpu_training/models.py
+++ 1_multi_gpu_training/models.py
@@ -20,74 +20,108 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import numpy as np
+import os.path as osp
 import pytorch_lightning as pl
-from pytorch_lightning.utilities.cli import MODEL_REGISTRY
 import torch
 import torch.nn as nn
-import torch_geometric as pyg
 import torch_optimizer as optim
 import torchmetrics.functional as F
-import os
+from typing import List, Tuple, Union
+import sys
+
+sys.path.append(osp.abspath('..'))
 
 import config
 
 class ThreeDCorrectionModule(pl.LightningModule):
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.net(x)
     
-    def _common_step(self, batch, batch_idx, stage):
+    def _common_step(self, 
+                     batch: List[torch.Tensor], 
+                     batch_idx: int, 
+                     stage: Union['train', 'test', 'val']) -> Tuple[torch.Tensor]:
+        
         x, y = batch
-        y_hat = self(x, edge_index)
+        y_hat = self(x)
+        
+        # Note: by using a metric from TorchMetrics, you don't have to manually set the sync_dist option, 'ensures that each GPU worker has the same behaviour when tracking model checkpoints, which is important for later downstream tasks such as testing the best checkpoint across all workers'.
         loss = F.mean_squared_error(y_hat, y)
-        self.log(f"{stage}_loss", loss, prog_bar=True, on_step=True, batch_size=len(batch))
+        self.log(
+            f"{stage}_loss", 
+            loss, prog_bar=True, 
+            on_step=True, 
+            batch_size=len(batch),
+            # Uncomment, for the 'test' and 'val' stages if you're not using TorchMetrics
+            # sync_dist = True
+        )
         
         return y_hat, loss
     
-    def training_step(self, batch, batch_idx):
+    def training_step(self, 
+                      batch: List[torch.Tensor], 
+                      batch_idx: int) -> torch.Tensor:
+        
         _, loss = self._common_step(batch, batch_idx, "train")
+        
         return loss
 
-    def validation_step(self, batch, batch_idx):
+    def validation_step(self, 
+                        batch: List[torch.Tensor], 
+                        batch_idx: int) -> Tuple[torch.Tensor]:
+        
         y_hat, _ = self._common_step(batch, batch_idx, "val")
 
-    def test_step(self, batch, batch_idx):
+    def test_step(self, 
+                  batch: List[torch.Tensor], 
+                  batch_idx: int) -> Tuple[torch.Tensor]:
         y_hat, _ = self._common_step(batch, batch_idx, "test")
     
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> optim.Optimizer:
+        
         return optim.AdamP(self.parameters(), lr=self.lr)
-
-
-@MODEL_REGISTRY
+    
+    
 class LitMLP(ThreeDCorrectionModule):
     
     class Normalize(nn.Module):
         
-        def __init__(self):
+        def __init__(self, module_instance) -> None:
+            
             super().__init__()
-            self.epsilon = torch.tensor(1.e-8)
+            
+            # 'The LightningModule knows what device it is on. You can access the reference via self.device. Sometimes it is necessary to store tensors as module attributes. However, if they are not parameters they will remain on the CPU even if the module gets moved to a new device. To prevent that and remain device agnostic, register the tensor as a buffer in your modules’s __init__ method with register_buffer().'
+            self._module = module_instance
+            self._module.register_buffer("epsilon", torch.tensor(1.e-8))
             
             step = config.config['timestep']
-            stats = torch.load(os.path.join(config.data_path, f"stats-{step}.pt"))
+            stats = torch.load(osp.join(config.data_path, f"stats-flattened-{step}.pt"))
             
             self.x_mean = stats["x_mean"]
             self.x_std = stats["x_std"]
             
-        def forward(self, x: torch.Tensor):
+        def forward(self, x: torch.Tensor) -> torch.Tensor:
+            
+            # Adjusting the tensors to scale to other devices. 'When you need to create a new tensor, use type_as. This will make your code scale to any arbitrary number of GPUs or TPUs with Lightning.'
+            mean = self.x_mean.type_as(x)
+            std = self.x_std.type_as(x)
 
-            return (x - self.x_mean) / (self.x_std + self.epsilon)
+            return (x - mean) / (std + self._module.epsilon)
+    
     
     def __init__(self, 
                  in_channels: int, 
                  hidden_channels: int, 
                  out_channels: int, 
-                 lr: float):
+                 lr: float = 1e-4) -> None:
+        
         super().__init__()
         
         self.lr = lr
+        
         self.net = nn.Sequential(
-            self.Normalize(),
+            LitMLP.Normalize(self),
             nn.Linear(in_channels, hidden_channels),
             nn.SiLU(),
             nn.Linear(hidden_channels, hidden_channels),

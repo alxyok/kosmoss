ccd3786b72946fde243423ba3ccfbdb8fde4f10f
alxyok <alxyok@naiama.com>
2022-03-03 14:06:43 +0000

    reran to double-check

--- src/kosmoss/dataproc/build_graphs.sh
+++ src/kosmoss/dataproc/build_graphs.sh
@@ -2,7 +2,7 @@
 
 export MAX_WORKERS=$(python -c "import psutil; print(psutil.cpu_count(logical=False))")
 
-USERNAME='mluser' python flows.py \
+USERNAME='mluser' python flows.py --no-pylint \
     run \
         --max-num-splits 7000 \
         --max-workers ${MAX_WORKERS} >> ${HOME}/.kosmoss/logs/build_graphs.stdout

3dae205d80a1f522cfb83a3eb6815d2cbee2bfe5
alxyok <alxyok@naiama.com>
2022-03-03 14:09:35 +0000

    paragraph on why no-pylint option

--- src/kosmoss/dataproc/build_graphs.sh
+++ src/kosmoss/dataproc/build_graphs.sh
@@ -2,6 +2,9 @@
 
 export MAX_WORKERS=$(python -c "import psutil; print(psutil.cpu_count(logical=False))")
 
+# Usually, you should enable pylint, really
+# But because PyTorch generates errors on its own, we'll simplify by just disabling it
+# Our code is clean though ;)
 USERNAME='mluser' python flows.py --no-pylint \
     run \
         --max-num-splits 7000 \

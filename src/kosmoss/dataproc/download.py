62ce9772d24f568a6cf92d3fbfab75a48d30fbf6
alxyok <alxyok@naiama.com>
2022-03-04 12:34:34 +0000

    tmp. fixed poetry install with dependencies, updated toml with scripts to download data and tfrecords

--- src/kosmoss/dataproc/download.py
+++ src/kosmoss/dataproc/download.py
@@ -1,83 +1,35 @@
 import climetlab as cml
-from distutils.errors import DistutilsArgError
 import os
 import os.path as osp
-from pathlib import Path
-from setuptools import Command
 
-from kosmoss import CACHED_DATA_PATH, DATA_PATH
-        
+from kosmoss import CACHED_DATA_PATH
+
 cml.settings.set("cache-directory", CACHED_DATA_PATH)
 cml.settings.set("number-of-download-threads", 16)
 
-class Download(Command):
-    
-    user_options = [
-        ('timestep=', 't', 'Temporal sampling step.'),
-    ]
-    
-    def initialize_options(self):
-        self.timestep = None
-        
-    def finalize_options(self):
-        if not self.timestep:
-            raise DistutilsArgError("You must specify --timestep option")
-    
-    def run(self):
-        
-        cml.load_dataset(
-            name="maelstrom-radiation",
-            dataset="3dcorrection",
-            timestep=list(range(0, 3501, int(self.timestep))),
-            raw_inputs=False,
-        )
+def download():
+    cml.load_dataset(
+        'maelstrom-radiation', 
+        dataset='3dcorrection', 
+        raw_inputs=False, 
+        timestep=list(range(0, 3501, 250)), 
+        minimal_outputs=False,
+        patch=list(range(0, 16, 1)),
+        hr_units='K d-1',
+    )
 
-class ConvertTFRecord(Command):
+def download_tfrecords():
+    cmlds = cml.load_dataset(
+        'maelstrom-radiation-tf',
+        dataset='3dcorrection',
+        timestep=list(range(0, 3501, 500)), 
+        filenum=list(range(5)),
+        norm=True,
+        hr_units="K d-1",
+    )
+    cmlds.to_tfdataset(batch_size=256)
     
-    user_options = [
-        ('timestep=', 't', 'Temporal sampling step.'),
-        ('batchsize=', 'b', 'Batch size.'),
-    ]
+if __name__ == "__main__":
     
-    def initialize_options(self):
-        self.timestep = None
-        self.batchsize = 256
-        
-    def finalize_options(self):
-        if not self.timestep or int(self.timestep) < 500:
-            raise DistutilsArgError("You must specify --timestep option >= 500")
-    
-    def run(self):
-        
-        cmlds = cml.load_dataset(
-            name="maelstrom-radiation-tf",
-            dataset="3dcorrection",
-            timestep=list(range(0, 3501, int(self.timestep))),
-            filenum=list(range(5)),
-            hr_units="K d-1",
-        )
-        cmlds.to_tfdataset(batch_size=self.batchsize)
-
-# class LinkCache(Command):
-    
-#     user_options = [
-#         ('srcpath=', 'p', "Path for the source 'cached' data dir."),
-#     ]
-    
-#     def initialize_options(self):
-#         self.srcpath = None
-        
-#     def finalize_options(self):
-#         if not self.srcpath:
-#             raise DistutilsArgError("You must specify --srcpath option")
-    
-#     def run(self):
-        
-#         os.makedirs(DATA_PATH, exist_ok=True)
-        
-#         if osp.isdir(CACHED_DATA_PATH):
-#             print(f"directory: {CACHED_DATA_PATH} already exists, failing gracefully...")
-#             return
-            
-#         path = Path(self.srcpath).resolve()
-#         os.symlink(path, CACHED_DATA_PATH, target_is_directory=True)
+    download()
+    download_tfrecords()
